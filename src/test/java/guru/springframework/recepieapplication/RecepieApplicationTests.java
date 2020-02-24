1db164103c98fcefc7a65773ff450292b726f912
InquisitorGor <egorayubdzhanov@gmail.com>
2020-02-24 10:46:49 +0300

    Created project from Spring.Initializer

---
+++ src/test/java/guru/springframework/recepieapplication/RecepieApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.recepieapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
